4392c6d074eb86b2b37e59ae29ecae727508532c
Dan Maglasang <dcmaglasang@gmail.com>
2017-05-17 12:33:34 -0400

    Update deps

--- blueprints/auth0-ember-simple-auth/index.js
+++ blueprints/auth0-ember-simple-auth/index.js
@@ -5,7 +5,7 @@ module.exports = {
   afterInstall: function() {
     return this.addBowerPackagesToProject([
       { name: 'ember-simple-auth',      target: '0.8.0'  },
-      { name: 'auth0-lock',             target: '~7.7.1'  },
+      { name: 'auth0-lock',             target: '~10.13.0'  },
       { name: 'jsrsasign',              target: '~4.8.3'  }
     ]);
   }
--- index.js
+++ index.js
@@ -6,7 +6,7 @@ module.exports = {
 
   included: function(app) {
     app.import(app.bowerDirectory + '/ember-simple-auth/simple-auth.amd.js');
-    app.import(app.bowerDirectory + '/auth0-lock/build/auth0-lock.js');
+    app.import(app.bowerDirectory + '/auth0-lock/build/lock.js');
     app.import(app.bowerDirectory + '/jsrsasign/jsrsasign-latest-all-min.js');
   },
 
